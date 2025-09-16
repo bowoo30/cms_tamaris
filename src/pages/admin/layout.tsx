d997cd99c2628ec88d2af36a7741494664f77daa
Syamsul Wibowo <bowoo30@gmail.com>
2025-09-16 14:16:46 +0700

    removea admin layout

--- src/pages/admin/layout.tsx
+++
@@ -1,7 +0,0 @@
-export const AdminLayout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div className="admin-layout">
-            {children}
-        </div>
-    );
-}
